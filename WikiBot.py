762261923d1570c509d2ab9d31d302efc5cee28f
Javier Toja <javier.toja.alamancos@gmail.com>
2016-09-05 10:51:32 +0200

    Update WikiBot.py
    unify py archives

--- WikiBot.py
+++ WikiBot.py
@@ -25,15 +25,49 @@ Created on 2 sept. 2016
 
 @author: Javier Toja Alamancos
 '''
-from flask_restful import Resource, reqparse
+from flask_restful import reqparse, Resource
+import requests
+import json
+from flask import Flask
+from flask_restful import Api
+import argparse
 
 class WikiBot(Resource):
     
-    def __init__(self):
+    def __init__(self,**kwargs):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('text')
+        self.channel = kwargs['channel']
+        self.hook = kwargs['hook']
+        self.message = kwargs['message']
 
     def post(self):
         args = self.parser.parse_args()
-        print("adiendo entrada a la wiki -- ["+args["text"]+"]")
-        return  {"text":"Entrada realizada correctamente !"}
+        payload = {"text": "[WIKI] "+args["text"], "channel": self.channel,"username": "wikiBot", "icon_url": "https://d13yacurqjgara.cloudfront.net/users/10958/screenshots/271458/librarian.jpg"}
+        requests.post(self.hook, json.dumps(payload) , verify = False)        
+        return  {"text": self.message, "response_type":"in_channel"}, 200
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='WikiBot 1.0')
+    
+    # Add arguments
+    parser.add_argument(
+        '-lp', '--listenIp', type=str, help='Ip linstening range', required=False, default='127.0.0.1')
+    parser.add_argument(
+        '-p', '--port', type=int, help='Port number', required=True)
+    parser.add_argument(
+        '-c', '--channel', type=str, help='Mattermos channel to respond', required=False, default='wiki')
+    parser.add_argument(
+        '-hk', '--hook', type=str, help='Mattermos hook url', required=True)
+    parser.add_argument(
+        '-m', '--message', type=str, help='Thanks message', required=False, default='Gracias por registrar su entrada')    
+    
+    args = parser.parse_args()
+
+    app = Flask(__name__)
+    api = Api(app)
+
+    api.add_resource(WikiBot, '/wikibot/', resource_class_kwargs={'channel': args.channel,'hook':args.hook, 'message':args.message})
+    app.run(args.listenIp,args.port)
